2ab7f246a0a80a43d5ee811d29d593b493093016
Krrish Mittal <takyyon@Krrishs-MacBook-Pro.local>
2022-06-17 12:06:50 -0700

    update port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const express = require("express");
 import {  getFrameworks, getRecommendation } from './lib/functions';
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 
 app.get('/', async (req: Request, res: Response) => {
   const repo = req.query.repo;
